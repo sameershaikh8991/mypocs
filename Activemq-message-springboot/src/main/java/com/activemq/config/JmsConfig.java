ac5ae58bd3e234549d08cf1efc4ad5c58168477c
sameershaikh <43517756+sameershaikh8991@users.noreply.github.com>
2023-04-07 12:29:28 +0530

    rabbit mq

---
+++ Activemq-message-springboot/src/main/java/com/activemq/config/JmsConfig.java
@@ -0,0 +1,2 @@
+package com.activemq.config;public class JmsConfig {
+}
---
+++ rabbitmq-message-springboot/src/main/java/com/rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,38 @@
+package com.rabbitmq.publisher;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private  String exchange;
+
+
+    @Value("${rabbitmq.routing.key}")
+    private  String routingKey;
+
+
+
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public  void sendMessage(String message){
+
+        log.info(String.format("Message sent - >%s",message));
+
+        rabbitTemplate.convertAndSend(exchange, routingKey,message);
+
+    }
+}
+
