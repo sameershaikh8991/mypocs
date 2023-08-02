63f63bfd0ab38d121d3f08c5c3ab86eb28e0e1de
sameershaikh <43517756+sameershaikh8991@users.noreply.github.com>
2023-08-02 18:59:23 +0530

    kafka-consumer on docker

---
+++ consumer/src/main/java/com/consumer/kafka/KafkaConsumer.java
@@ -0,0 +1,17 @@
+package com.consumer.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(String message){
+        System.out.println(String.format("Message received -> %s", message));
+        log.info(String.format("Message received -> %s", message));
+    }
+}
